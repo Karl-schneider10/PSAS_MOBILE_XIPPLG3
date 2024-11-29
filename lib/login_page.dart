616496402cfd05ddad5b7ae390659067b2bd875b
Karl-schneider10 <andikamarhas@gmail.com>
2024-11-29 11:04:19 +0700

    pertama

---
+++ lib/login_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/butterfly.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      controller: _passwordController,
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          padding: const EdgeInsets.all(12),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        onPressed: () {
+          if (_emailController.text.isNotEmpty &&
+              _passwordController.text.isNotEmpty) {
+            Navigator.of(context).pushNamed(HomePage.tag);
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Email dan Password tidak boleh kosong')),
+            );
+          }
+        },
+        child: const Text('Log In', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final forgotLabel = TextButton(
+      child: const Text(
+        'Lupa password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {
+        // Tambahkan logika untuk reset password
+      },
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            logo,
+            const SizedBox(height: 48.0),
+            email,
+            const SizedBox(height: 8.0),
+            password,
+            const SizedBox(height: 24.0),
+            loginButton,
+            forgotLabel,
+          ],
+        ),
+      ),
+    );
+  }
+}
